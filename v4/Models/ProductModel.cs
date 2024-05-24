0f06a09212df3d3c6d5e077daee5916c0472470a
senih1 <164724567+senih1@users.noreply.github.com>
2024-05-24 19:59:46 +0300

    v4

---
+++ v4/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrunSatisApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public int Stock { get; set; }
+    }
+
+    public class Cart
+    {
+        public string Name { get; set; }
+        public int PaidPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
