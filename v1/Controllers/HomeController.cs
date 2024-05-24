b61d4f695353aa0ba13bc89ce876edad9d4f1842
senih1 <164724567+senih1@users.noreply.github.com>
2024-05-24 19:00:57 +0300

    v1

---
+++ v1/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using UrunSatisApp.Models;
+
+namespace UrunSatisApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Product> LoadProducts()
+        {
+            var products = new List<Product>();
+            using StreamReader productReader = new StreamReader("App_Data/products.txt");
+            string productsTxt = productReader.ReadToEnd();
+
+            if (!string.IsNullOrEmpty(productsTxt))
+            {
+                var productList = productsTxt.Split('\n');
+
+                foreach (var productLine in productList)
+                {
+                    var product = productLine.Split('|');
+                    products.Add
+                 (
+                    new Product
+                    {
+                        Name = product[0],
+                        Price = int.Parse(product[1]),
+                        Image = product[2]
+                    }
+                 );
+                }
+            }
+            return products;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = LoadProducts();
+            return View(products);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Cart model)
+        {
+            var products = LoadProducts();
+
+            var choosenProduct = new Product();
+            foreach (var product in products)
+            {
+                if(product.Name == model.Name)
+                {
+                    choosenProduct = product;
+                    break;
+                }
+            }
+
+            var changeBack = model.PaidPrice - choosenProduct.Price;
+            var incomplete = choosenProduct.Price - model.PaidPrice;
+
+            if (changeBack > 0)
+            {
+                ViewData["Msg"] = $"<div class=\"alert alert-success\" role=\"alert\">\r\n  Satın alımınız için teşekkür ederiz! Para üstünüz : {changeBack} TL\r\n</div>\r\n";
+
+            }
+            else if (changeBack < 0)
+            {
+                ViewData["Msg"] = $"<div class=\"alert alert-warning\" role=\"alert\">\r\n  Eksik ödeme yaptınız! Eklenmesi gereken tutar : {incomplete} TL\r\n</div>\r\n";
+            }
+            else
+            {
+                ViewData["Msg"] = $"<div class=\"alert alert-success\" role=\"alert\">\r\n  Satın alımınız için teşekkür ederiz!\r\n</div>\r\n";
+            }
+
+            return View(products);
+        }
+    }
+}
---
+++ v1/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrunSatisApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class Cart
+    {
+        public string Name { get; set; }
+        public int PaidPrice { get; set; }
+    }
+}
